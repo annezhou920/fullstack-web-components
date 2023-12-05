54dde3755e271eec9b3eb8785316bde885b505b6
Anne Zhou <annez@zillowgroup.com>
2023-12-05 15:22:20 -0500

    shadow styling

--- packages/component/src/card/Card.ts
+++ packages/component/src/card/Card.ts
@@ -5,11 +5,38 @@ export class CardComponent extends HTMLElement {
     const shadowRoot = this.attachShadow({ mode: 'open' });
     const template = document.createElement('template');
     template.innerHTML = `
-        <span>
+        <div>
+            <style>
+            :host {
+                display: block;
+                background: var(--color-white); 
+                border-radius: var(--radius-md); 
+                box-shadow: var(--shadow); 
+                overflow: hidden;
+                max-width: 320px;
+            }
+
+            ::slotted(*) {
+                padding: 0 var(--padding-lg);
+            }
+
+            ::slotted(a:link), ::slotted(a:visited) {
+                display: block;
+            }
+
+            ::slotted(:last-child) {
+                padding-bottom: var(--margin-lg);
+            }
+
+            ::slotted(img) {
+                width: 100%;
+                padding: 0;
+            }
+            </style>
             <header><slot name="header"></slot></header>
             <section><slot name="content"></slot></section>
             <footer><slot name="footer"></slot></footer>
-        </span>
+        </div>
     `;
     shadowRoot.appendChild(template.content.cloneNode(true));
   }
